6645f39281cd947686da3564448f515d2a5755f1
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-01-30 21:12:00 -0500

    with the changes

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Biswash Adhikari on 1/28/18.
+//  Copyright © 2018 Biswash Adhikari. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
