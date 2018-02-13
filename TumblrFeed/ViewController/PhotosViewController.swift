262d7550a280417d89b049dbd843c9777d94d165
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-02-13 10:44:14 -0500

    week 2

---
+++ TumblrFeed/ViewController/PhotosViewController.swift
@@ -0,0 +1,165 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Pranaya Adhikari on 1/27/18.
+//  Copyright © 2018 Pranaya Adhikari. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var posts: [[String: Any]] = []
+    var refreshControl = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 217
+        
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        self.tableView.insertSubview(refreshControl, at: 0)
+        
+        loadImages()
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func refreshControlAction(_ refreshControl: UIRefreshControl){
+        loadImages()
+        refreshControl.endRefreshing()
+    }
+    
+    func loadImages() {
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                let alertController = UIAlertController(title: "Cannot Get Images", message: "The internet connection appears to be offline", preferredStyle: .alert)
+                // create a cancel action
+                let cancelAction = UIAlertAction(title: "Try Again", style: .cancel) { (action) in
+                    self.loadImages()
+                    // handle cancel response here. Doing nothing will dismiss the view.
+                }
+                alertController.addAction(cancelAction)
+                self.present(alertController, animated: true){
+                    
+                }
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                //print(dataDictionary)
+                
+                //get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String:Any]
+                
+                //store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String:Any]]
+                self.tableView.reloadData()
+                
+                
+                // TODO: Get the posts and store in posts property
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell") as! PhotoCell
+        
+        let post = posts[indexPath.section]
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            
+            cell.photoView.af_setImage(withURL: url!)
+            
+        }
+        
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let post = posts[indexPath.section]
+            let photos = post["photos"] as? [[String: Any]]
+            let photo = photos![0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let vc = segue.destination as! PhotoDetailsViewController
+            vc.imageUrl = urlString
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.posts.count
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).cgColor
+        profileView.layer.borderWidth = 1;
+        
+        profileView.af_setImage(withURL: URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/avatar")!)
+        headerView.addSubview(profileView)
+        
+        let post = posts[section]
+        let date = post["date"] as! String
+        let headerLabel = UILabel(frame: CGRect(x: 50, y: 12, width:
+            tableView.bounds.size.width, height: tableView.bounds.size.height))
+        headerLabel.font = UIFont(name:"Roboto" , size: 15)
+        headerLabel.textColor = UIColor.black
+        headerLabel.text = date
+        headerLabel.sizeToFit()
+        headerView.addSubview(headerLabel)
+        
+        return headerView
+        
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
